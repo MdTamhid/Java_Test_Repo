15d9754e0abba84eee241f6551600fc6ed8d4fa1
Tamhid <Tamhid@Tamhid>
2024-10-05 11:19:31 +0530

    updated

---
+++ Test/src/ClassA.java
@@ -0,0 +1,13 @@
+
+public class ClassA {
+	
+	public static void main(String[] args) {
+		runSpeed(90);
+	}
+
+	
+	public static void runSpeed(int speed) {
+		
+		System.out.println("Bike running speed "+speed);
+	}
+}
